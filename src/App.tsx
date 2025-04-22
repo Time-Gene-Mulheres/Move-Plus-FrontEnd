1f72d31a1bee2965118e0bb9d3fa9407ec087225
alexaniatoma <alexaniatoma@hotmail.com>
2025-04-22 15:25:31 -0300

    implementacao da branch treino

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import ListaCategorias from './components/categoria/listacategoria/ListaCategorias'
 import Footer from './components/footer/Footer'
+import ListaTreinos from './components/treinos/listatreinos/ListaTreinos'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/categorias' element={<ListaCategorias/>}/>
         {/* rota Treino apenas para visualização*/}
+        <Route path="/treinos" element={<ListaTreinos/>} />
         {/* rota usuario */}
       </Routes>
       <Footer/>
