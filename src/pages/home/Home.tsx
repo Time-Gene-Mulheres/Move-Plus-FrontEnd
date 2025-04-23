c338dd58009721aabd0be025344ec570abd49f12
KeifSant <keyllafsantos1@gmail.com>
2025-04-23 10:53:33 -0300

    link novo treino pag Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 function Home() {
     return (
@@ -17,7 +18,7 @@ function Home() {
                             <div className='rounded text-[#e9c46a]
                                             border-[#e9c46a] border-solid border-2 py-2 px-4'
                                 >
-                                Novo Treino
+                            <Link to='/novoTreino'>Novo Treino</Link>
                             </div>
                         </div>
                     </div>
