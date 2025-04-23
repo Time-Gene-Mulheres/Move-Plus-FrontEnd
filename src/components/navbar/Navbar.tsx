062a3618a3105cb682519ab06e1d8fe8f9f3ef8f
Gabrielly Barbosa Silva <gabriellybarbosa.dev@gmail.com>
2025-04-23 16:24:34 -0300

    ajustando rota

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,24 +2,25 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
     return (
-        <>
+        <div>
             <div className='w-full flex justify-between items-center px-8 py-4
             bg-[#264653] text-[#e9c46a]'>
             
                 <div className="container flex justify-between text-lg">
-                <Link to='/home' className="text-2xl font-bold">
-                  Move Plus
-                </Link>
+                    <Link to='/home' className="text-2xl font-bold">
+                        Move Plus
+                    </Link>
 
                     <div className='flex gap-4 text-lg'>
-                        <Link to='/usuario'>Usuário</Link>
+                        <Link to='/usuarios'>Usuário</Link>
                         <Link to='/cadastrartreinos'>Cadastrar Treino</Link>
                         <Link to='/cadastrarcategoria'>Cadastrar Categoria</Link>
                         <Link to='/login'>Sair</Link>
                     </div>
+                </div>
             </div>
             
-        </>
+        </div>
     )
 }
 
